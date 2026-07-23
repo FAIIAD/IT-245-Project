51102989d542e01b8d37bf70077c18f7ac68aa26
FAIIAD <xskytears@gmail.com>
2026-07-24 01:48:58 +0300

    Create Book class

---
+++ BookInquirySystem.java
@@ -0,0 +1,21 @@
+class Book implements Comparable {
+    String isbn;
+    String title;
+    String author;
+
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public int compareTo(Book other) {
+        return this.isbn.compareTo(other.isbn);
+    }
+
+    @Override
+    public String toString() {
+        return "'" + title + "' by " + author;
+    }
+}
