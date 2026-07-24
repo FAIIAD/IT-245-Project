497ff24039e1c1b751480800d2e64ec3862981cb
rawaf260 <rawafsalem@gmail.com>
2026-07-24 18:22:16 +0300

    Create Node.java
    add Node class for binary search tree

---
+++ src/bookInquierySystemUsingBTS/Node.java
@@ -0,0 +1,13 @@
+package bookInquierySystemUsingBTS;
+
+public class Node {
+    Book book;
+    Node left;
+    Node right;
+
+    public Node(Book book) {
+        this.book = book;
+        this.left = null;
+        this.right = null;
+    }
+}
