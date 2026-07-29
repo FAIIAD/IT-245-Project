7129e10a904678bf0edb562e7c1cd9c492eec0ff
FAIIAD <xskytears@gmail.com>
2026-07-29 11:17:28 +0300

    Update Book.java
    Made the code clear

--- src/bookInquirySystem/Book.java
+++ src/bookInquirySystem/Book.java
@@ -1,23 +1,26 @@
 package bookInquirySystem;
-
+// Comparable method is used in order to conduct BinarySearchTree 
 public class Book implements Comparable<Book> {
+	// Book attributes 
     String isbn;
     String title;
     String author;
-
+// This is Constructor used to build books when given information 
     public Book(String isbn, String title, String author) {
         this.isbn = isbn;
         this.title = title;
         this.author = author;
     }
 
+    // This is used to decide if book goes to right node or left node based on alphabetical order
     @Override
     public int compareTo(Book other) {
-        return this.isbn.compareTo(other.isbn);
+        return this.title.compareTo(other.title);
     }
 
+    // Print statement shown on console
     @Override
     public String toString() {
-        return "'" + title + "' by " + author;
+        return "Title: '" + title + "', Author: '" + author + "'";
     }
 }
