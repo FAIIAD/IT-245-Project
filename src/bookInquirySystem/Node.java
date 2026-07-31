11a8dc8e5d95b9f4c67b3aff17132d369feb1d1d
FAIIAD <xskytears@gmail.com>
2026-07-31 19:18:09 +0300

    Clarifying Node.java

--- src/bookInquirySystem/Node.java
+++ src/bookInquirySystem/Node.java
@@ -1,10 +1,15 @@
 package bookInquirySystem;
 
 public class Node {
+	//book will have actual book information 
     Book book;
+    //Nodes are like pointers. it will sort items according to book title.
+    // book will be stored in left node if title starting with a letter smaller than current node. 
     Node left;
+    // book will be stored in right node if title starting with a letter larger than current node. 
     Node right;
 
+    //this is a constructor used to store Book value inside the Node
     public Node(Book book) {
         this.book = book;
         this.left = null;
