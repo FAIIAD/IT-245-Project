8245c9981cc97fde35c62cc4eee58b4b46fdf1bd
sultan <sultan86481@gmail.com>
2026-08-01 15:14:45 +0300

    Refactor comments in Node class
    Updated comments for clarity on node structure and book storage.

--- src/bookInquirySystem/Node.java
+++ src/bookInquirySystem/Node.java
@@ -3,11 +3,11 @@ package bookInquirySystem;
 public class Node {
 	//book will have actual book information 
     Book book;
-    //Nodes are like pointers. it will sort items according to book title.
-    // book will be stored in left node if title starting with a letter smaller than current node. 
-    Node left;
-    // book will be stored in right node if title starting with a letter larger than current node. 
-    Node right;
+    // Books with alphabetically smaller titles are stored in the left subtree.
+Node left;
+
+// Books with alphabetically greater titles are stored in the right subtree.
+Node right;
 
     //this is a constructor used to store Book value inside the Node
     public Node(Book book) {
